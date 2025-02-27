12d2cbfa355450eee2e155dc1c66543fa854a6d2
skmoin97 <skmoinuddin1997@gmail.com>
2025-02-27 12:31:34 +0530

    Add files via upload
    All files are uploaded successfully.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// ========JS for Navbar Menu Button=============
+let mainListDiv = document.querySelector(".nav-menu"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("fa-x");
+    
+};
+
+
+// ===========JS for Accordion button====================
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";  
+    } 
+  });
+}
